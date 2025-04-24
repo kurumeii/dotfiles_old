dcbe23fe9f86e8a25c02ee77c46d180b44393629
Hoang Anh <ngphuchoanganh@gmail.com>
2025-04-24 11:12:08 +0700

    feat(astro): Add AstroNvim

---
+++ astronvim/README.md
@@ -0,0 +1,12 @@
+# My Astro Nvim configuration
+
+## Things TODO:
+
+1. Change key binding
+- [ ] For LSP
+- [ ] For Picker
+
+2. Override/ Disable core plugins
+- [ ] FileTree
+- [ ] Which Key GUI 
+- [ ] Use different LSP (?)
---
+++ astronvim/lua/community.lua
@@ -0,0 +1,8 @@
+---@type LazySpec
+return {
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.colorscheme.tokyonight-nvim" },
+	{ import = "astrocommunity.colorscheme.kanagawa-nvim" },
+	-- import/override with your plugins folder
+}
