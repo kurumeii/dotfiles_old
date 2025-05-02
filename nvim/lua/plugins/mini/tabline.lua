e0cf5626a72d9dea20f3a2aa651adbb63e3d0552
Hoang Anh <ngphuchoanganh@gmail.com>
2025-05-02 14:35:04 +0700

    fix(nvim): tabline
    Add parent path (experimental)

--- nvim/lua/plugins/mini/tabline.lua
+++ nvim/lua/plugins/mini/tabline.lua
@@ -2,9 +2,10 @@
 require('mini.tabline').setup({
   show_icon = true,
   format = function(buf_id, label)
-    local defualt_fm = MiniTabline.default_format(buf_id, label)
+		local parent = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(buf_id), ':h:t') or ''
+    local default_format = MiniTabline.default_format(buf_id, parent .. '/' .. label)
     local suffix = vim.bo[buf_id].modified and '󰳻 ' or ''
-    return defualt_fm .. suffix
+    return default_format .. suffix
   end,
 })
 
