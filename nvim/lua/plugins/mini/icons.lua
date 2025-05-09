83ba1b2b24bd7901cd6c827749d8a285b12e5625
Hoang Anh <ngphuchoanganh@gmail.com>
2025-05-09 10:24:36 +0700

    feat(nvim): Add custom icons

--- nvim/lua/plugins/mini/icons.lua
+++ nvim/lua/plugins/mini/icons.lua
@@ -9,6 +9,19 @@ require('mini.icons').setup({
     ['tsconfig.json'] = { glyph = '', hl = 'MiniIconsAzure' },
     ['tsconfig.build.json'] = { glyph = '', hl = 'MiniIconsAzure' },
     ['yarn.lock'] = { glyph = '', hl = 'MiniIconsBlue' },
+    ['vite.config.ts'] = { glyph = '', hl = 'MiniIconsYellow' },
+    ['pnpm-lock.yaml'] = { glyph = '', hl = 'MiniIconsYellow' },
+    ['pnpm-workspace.yaml'] = { glyph = '', hl = 'MiniIconsYellow' },
+    ['.dockerignore'] = { glyph = '󰡨', hl = 'MiniIconsBlue' },
+    ['react-router.config.ts'] = { glyph = '', hl = 'MiniIconsRed' },
+  },
+  directory = {
+    ['.vscode'] = { glyph = '', hl = 'MiniIconsBlue' },
+    ['app'] = { glyph = '󰀻', hl = 'MiniIconsRed' },
+    ['routes'] = { glyph = '󰑪', hl = 'MiniIconsGreen' },
+    ['config'] = { glyph = '', hl = 'MiniIconsGrey' },
+    ['configs'] = { glyph = '', hl = 'MiniIconsGrey' },
+    ['server'] = { glyph = '󰒋', hl = 'MiniIconsCyan' },
   },
 })
 MiniIcons.mock_nvim_web_devicons()
