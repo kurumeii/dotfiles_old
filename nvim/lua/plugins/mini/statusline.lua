9660c3470da4d475dfedf3329b719e45f5e2285c
Hoang Anh <ngphuchoanganh@gmail.com>
2025-05-09 19:43:26 +0700

    refactor(nvim): Statusline
    - Better code structure
    - At function to recognize percentage or regular line

--- nvim/lua/plugins/mini/statusline.lua
+++ nvim/lua/plugins/mini/statusline.lua
@@ -1,11 +1,11 @@
 local MiniStatusline = require('mini.statusline')
 local battery = require('battery')
 
-function _G.statusline_time()
+local function get_time()
   return mininvim.icons.clock .. ' ' .. os.date('%R')
 end
 
-function _G.recorder()
+local function recorder_section()
   local reg = vim.fn.reg_recording()
   if reg ~= '' then
     return mininvim.icons.recording .. ' ' .. reg
@@ -13,9 +13,39 @@ function _G.recorder()
   return ''
 end
 
+local function get_lsp()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local clients = vim.lsp.get_clients({ bufnr = bufnr })
+  if #clients == 0 then
+    return '󱏎 No LSP'
+  end
+  local names = {}
+  for _, client in ipairs(clients) do
+    table.insert(names, client.name)
+  end
+  return mininvim.icons.lsp .. ' ' .. table.concat(names, ',')
+end
+
+--- @param mode 'percent' | 'line'
+local function get_location(mode)
+  if mode == 'percent' then
+    local current_line = vim.api.nvim_win_get_cursor(0)[1]
+    local total_lines = vim.api.nvim_buf_line_count(0)
+    if current_line == 1 then
+      return 'TOP'
+    elseif current_line == total_lines then
+      return 'BOTTOM'
+    else
+      return '%p%%'
+    end
+  else
+    return '%l|%v'
+  end
+end
+
 local function active_mode()
   local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 250 })
-  mode = mininvim.icons.mode_prepend .. ' ' .. mode:gsub('%s+$', '')
+  mode = mininvim.icons.mode_prepend .. ' ' .. mode
 
   local git = MiniStatusline.section_git({ icon = mininvim.icons.git_branch, trunc_width = 40 })
   local diff = MiniStatusline.section_diff({ icon = '', trunc_width = 100 })
@@ -29,28 +59,13 @@ local function active_mode()
     },
     trunc_width = 75,
   })
-  MiniStatusline.section_lsp = function()
-    local bufnr = vim.api.nvim_get_current_buf()
-    local clients = vim.lsp.get_clients({ bufnr = bufnr })
-    if #clients == 0 then
-      return '󱏎 No LSP'
-    end
-    local names = {}
-    for _, client in ipairs(clients) do
-      table.insert(names, client.name)
-    end
-    return mininvim.icons.lsp .. ' ' .. table.concat(names, ',')
-  end
-  local lsp = MiniStatusline.section_lsp()
+  local lsp = get_lsp()
   local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 200 })
-  MiniStatusline.section_location = function()
-    return '%l|%v'
-  end
-  local location = MiniStatusline.section_location()
+  local location = get_location('percent')
   local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
   local nvim_battery = battery.get_status_line()
-  local time = string.format('%s', statusline_time())
-  local recorder = string.format('%s', recorder())
+  local time = get_time()
+  local recorder = recorder_section()
   local filename = MiniStatusline.section_filename({ trunc_width = 250 })
   return MiniStatusline.combine_groups({
     { hl = mode_hl, strings = { mode } },
