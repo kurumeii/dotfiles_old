9cc1b6e35828ed100edabce6a39ea2a58480274d
Hoang Anh <ngphuchoanganh@gmail.com>
2025-05-29 15:08:03 +0700

    Chore(mini): Update package

--- mininvim/lua/plugins/theme/kanagawa.lua
+++ mininvim/lua/plugins/theme/kanagawa.lua
@@ -2,8 +2,5 @@ require('kanagawa').setup({
   compile = true,
   transparent = false,
   dimInactive = true,
-  background = {
-    light = 'lotus',
-    dark = 'dragon',
-  },
+  theme = 'dragon',
 })
