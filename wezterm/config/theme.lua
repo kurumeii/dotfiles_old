aea4abc6e5ce8742f19b0ea14f7379f4ada6c26e
Hoang Anh <ngphuchoanganh@gmail.com>
2025-06-22 22:34:48 +0700

    chore(wez): Turn off background blur

--- wezterm/config/theme.lua
+++ wezterm/config/theme.lua
@@ -1,9 +1,9 @@
 return {
 	color_scheme = "Kanagawa (Gogh)",
 	enable_scroll_bar = true,
-	window_background_opacity = 0.7,
-	macos_window_background_blur = 20,
-	win32_system_backdrop = "Acrylic", -- Mica | Acrylic | Tabbed
+	-- window_background_opacity = 0.7,
+	-- macos_window_background_blur = 20,
+	-- win32_system_backdrop = "Acrylic", -- Mica | Acrylic | Tabbed
 	hide_tab_bar_if_only_one_tab = true,
 	tab_bar_at_bottom = true,
 	use_fancy_tab_bar = false,
