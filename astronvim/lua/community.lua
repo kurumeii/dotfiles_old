2d9b994d7ecbe1353363b00c440056b436c44d83
Hoang Anh <ngphuchoanganh@gmail.com>
2025-06-25 18:27:46 +0700

    refactor(astro): remove

--- astronvim/lua/community.lua
+++
@@ -1,8 +0,0 @@
----@type LazySpec
-return {
-	"AstroNvim/astrocommunity",
-	{ import = "astrocommunity.pack.lua" },
-	{ import = "astrocommunity.colorscheme.tokyonight-nvim" },
-	{ import = "astrocommunity.colorscheme.kanagawa-nvim" },
-	-- import/override with your plugins folder
-}
