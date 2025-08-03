d8356fa9f66de7b31365b8f734b9ec7eee73e18f
Hoang Anh <ngphuchoanganh@gmail.com>
2025-08-03 21:58:57 +0700

    refactor(mini): tailwindcss
    - [x] Copy changes from lazynvim
    - [ ] Incorrect color (#35)

--- mininvim/lsp/tailwindcss.lua
+++ mininvim/lsp/tailwindcss.lua
@@ -1,19 +1,23 @@
-return {
-  filetypes = {
-    'html',
-    'css',
-    'javascript',
-    'javascriptreact',
-    'typescript',
-    'typescriptreact',
-  },
-  settings = {
-    tailwindCSS = {
-      classFunctions = {
-        'cva',
-        'cx',
-        'tv',
-      },
-    },
+local opts = {
+  excludes = { 'markdown' },
+  includes = {},
+  --- uncomment to fully override the default filetypes
+  -- filetypes = {}
+}
+
+local tw = require('lspconfig.configs.tailwindcss')
+opts.filetypes = opts.filetypes or {}
+
+vim.list_extend(opts.filetypes, tw.default_config.filetypes)
+
+opts.filetypes = vim.tbl_filter(function(ft)
+  return not vim.tbl_contains(opts.excludes or {}, ft)
+end, opts.filetypes)
+
+vim.list_extend(opts.filetypes, opts.includes or {})
+opts.settings = {
+  tailwindCSS = {
+    classFunctions = { 'cva', 'cx', 'tv' },
   },
 }
+return opts
