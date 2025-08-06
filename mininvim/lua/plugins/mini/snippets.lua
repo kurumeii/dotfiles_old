21eca5596aeaee60d69f2c74e90f8a7529141b3a
ext-andy-vo <ext-andy.vo@spdigital.sg>
2025-08-06 07:36:11 +0700

    Feat(mini): add snippet engine for js

--- mininvim/lua/plugins/mini/snippets.lua
+++ mininvim/lua/plugins/mini/snippets.lua
@@ -1,13 +1,35 @@
-local snippets = require('mini.snippets')
-snippets.setup({
-  snippets = {
-    snippets.gen_loader.from_lang(),
-    snippets.start_lsp_server(),
-  },
-  expand = {
-    select = function(snip, ins)
-      local select = snippets.default_select
-      select(snip, ins)
+local lang_patterns = {
+  jsx = { 'javascript/javascript.json', 'javascript/react-es7.json' },
+  tsx = { 'javascript/javascript.json', 'javascript/typescript.json', 'javascript/react-ts.json' },
+}
+mininvim.deps.setup({
+  {
+    source = 'rafamadriz/friendly-snippets',
+    cb = function()
+      vim.api.nvim_create_autocmd({ 'InsertEnter' }, {
+        pattern = '*',
+        callback = function()
+          local snippets, config_path = require('mini.snippets'), vim.fn.stdpath('config')
+          snippets.setup({
+            snippets = {
+              snippets.gen_loader.from_lang({
+                lang_patterns = lang_patterns,
+              }),
+              snippets.gen_loader.from_file(config_path .. '/snippets/global.json'),
+              snippets.start_lsp_server(),
+            },
+            mappings = {
+              expand = '',
+            },
+            expand = {
+              select = function(snip, ins)
+                local select = snippets.default_select
+                select(snip, ins)
+              end,
+            },
+          })
+        end,
+      })
     end,
   },
 })
