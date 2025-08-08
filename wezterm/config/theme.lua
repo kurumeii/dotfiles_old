5700e304f5a1082fbc97b2ee4a9654eb8ea661f7
andrew nguyen <andrew.nguyen.ext@ekimetrics.com>
2025-08-08 08:24:14 +0700

    refactor(wezterm): Tweak configuration

--- wezterm/config/theme.lua
+++ wezterm/config/theme.lua
@@ -4,7 +4,7 @@ return {
 	-- window_background_opacity = 0.7,
 	-- macos_window_background_blur = 20,
 	-- win32_system_backdrop = "Acrylic", -- Mica | Acrylic | Tabbed
-	hide_tab_bar_if_only_one_tab = true,
+	hide_tab_bar_if_only_one_tab = false,
 	tab_bar_at_bottom = true,
 	use_fancy_tab_bar = false,
 	window_decorations = "RESIZE",
